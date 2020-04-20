fe0ebf0fbc34e03c1e98a7420d32cacea69cc5b1
josephroyking <joey@bitcoin.com>
2020-04-20 11:13:39 -0700

    Exporting properly

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@
 
 export { default as BadgerButton } from './BadgerButton';
 export { default as BadgerBadge } from './BadgerBadge';
+export { default as Invoice } from './Invoice';
 export { default as PriceDisplay } from './PriceDisplay';
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@
 export * from './components';
 export * from './hoc';
 export * from './atoms';
-
 export * from './utils';
